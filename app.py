1cd4059d5ea20f9db908654a8491dd1e0c580d7d
hann <hann@hannweb.co>
2023-09-14 18:47:30 -0600

    project structure, navbar to skip login validation

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+# Configure database
 
 
 @app.after_request
@@ -25,12 +25,43 @@ def after_request(response):
     return response
 
 
-
 @app.route("/")
 def index():
+    """landing page when not logged in"""
     return render_template("index.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    """landing page when logged in"""
+    return render_template("dashboard.html")
 
 
 @app.route("/login")
 def login():
+    """log in"""
     return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    """sign up"""
+    return render_template("signup.html")
+
+
+@app.route("/new_product")
+def new_product():
+    """form to capture the new product"""
+    return render_template("new_product.html")
+
+
+@app.route("/list")
+def products_list():
+    """show list current or the one selected"""
+    return render_template("list.html")
+
+
+@app.route("/history")
+def history():
+    """show previous lists"""
+    return render_template("history.html")
